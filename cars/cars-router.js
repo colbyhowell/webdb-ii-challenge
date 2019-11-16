3d1a2e8de51f72f639fc97b0320bf7c532ec7d91
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-11-15 21:04:08 -0600

    got most of stretch done. Couldn't get the sales table done

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -27,8 +27,29 @@ router.post('/', (req, res) => {
         })
 })
 
-// router.put('/:id' (req, res) => {
-//     const changeDAta
-// })
+router.put('/:id', async (req, res) => {
+    const changeData = req.body
+    const { id } = req.params
+
+    try {
+        await db('cars-dealer').where({ id }).update(changeData)
+        const changedDb = await db('cars-dealer')
+        res.status(200).json(changedDb)
+    } catch{
+        res.status(500).json({ message: "This ID was not found" })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        await db('cars-dealer').where({ id }).del()
+        const newDb = await db('cars-dealer')
+        res.status(200).json(newDb)
+    } catch{
+        res.status(500).json({ message: "Item could not be deleted" })
+    }
+})
 
 module.exports = router
